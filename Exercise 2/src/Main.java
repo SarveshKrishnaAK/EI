fd250e8567f2e5c77d351867dfb282bf0f80eb69
sarveshkrishnaak <sarveshkrishnaak@gmail.com>
2025-09-30 20:36:24 +0530

    Removed previously existing Exercise 2 folder from repository as it was renamed to Exercise-2

--- Exercise 2/src/Main.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class Main {
-    public static void main(String[] args) {
-    System.out.println("Welcome to the Virtual Classroom Manager!");
-    VirtualClassroomApp app = new VirtualClassroomApp();
-    app.run();
-    }
-}
